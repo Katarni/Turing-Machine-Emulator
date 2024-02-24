97a2a383d5e51cabe386cd8a1d1c47a62b78e033
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-02-24 15:43:14 +0300

    right ear edition finished

--- include/TuringM.h
+++ include/TuringM.h
@@ -13,24 +13,33 @@ class TuringM : QMainWindow {
  public:
   TuringM();
 
-  void start();
-
  private slots:
   void edit_left_cont();
+  void edit_right_cont();
 
  private:
   const int width = 1500, height = 800;
-  const int close_ear_left_x = 390, close_ear_left_y = 150;
-  const int open_ear_left_x = 12, open_ear_left_y = 150;
-  const int open_cont_left_x = 90, open_cont_left_y = 125;
-  const int close_cont_left_x = 468, close_cont_left_y = 125;
+
+  const int close_ear_left_x = 370, close_ear_left_y = 150;
+  const int open_ear_left_x = -8, open_ear_left_y = 150;
+  const int open_cont_left_x = 70, open_cont_left_y = 125;
+  const int close_cont_left_x = 448, close_cont_left_y = 125;
+
+  const int close_ear_right_x = 860, close_ear_right_y = 150;
+  const int open_ear_right_x = 1233, open_ear_right_y = 150;
+  const int open_cont_right_x = 840, open_cont_right_y = 125;
+  const int close_cont_right_x = 462, close_cont_right_y = 125;
 
   bool left_opened = false;
+  bool right_opened = false;
 
   QMainWindow* window;
   QWidget* turing_head;
   QWidget* left_ear;
   QWidget* left_controller;
+  QWidget* right_ear;
+  QWidget* right_controller;
 
   QPushButton* open_left_controller;
+  QPushButton* open_right_controller;
 };
--- src/TuringM.cpp
+++ src/TuringM.cpp
@@ -13,11 +13,12 @@ TuringM::TuringM() {
 
   turing_head = new QWidget(window);
   turing_head->setFixedSize(700, 800);
-  turing_head->move(400, 0);
+  turing_head->move(380, 0);
   turing_head->setStyleSheet("QWidget { background: url(../img/turing.png);"
                              "background-position: center center;"
                              "background-repeat: no-repeat; }");
 
+  //// Left Side
   left_ear = new QWidget(window);
   left_ear->setFixedSize(220, 400);
   left_ear->move(close_ear_left_x, close_ear_left_y);
@@ -33,12 +34,35 @@ TuringM::TuringM() {
 
   open_left_controller = new QPushButton(window);
   open_left_controller->resize(50, 45);
-  open_left_controller->move(415, 353);
+  open_left_controller->move(395, 353);
   open_left_controller->setStyleSheet("QPushButton { background: transparent;"
                                                     "color: #184C01;"
                                                     "font-size: 45px; } ");
   open_left_controller->setText("<");
   connect(open_left_controller, SIGNAL(released()), this, SLOT(edit_left_cont()));
+
+  //// Right Side
+  right_ear = new QWidget(window);
+  right_ear->setFixedSize(220, 400);
+  right_ear->move(close_ear_right_x, close_ear_right_y);
+  right_ear->setStyleSheet("QWidget { background: url(../img/right_ear.png); }");
+  right_ear->stackUnder(turing_head);
+
+  right_controller = new QWidget(window);
+  right_controller->setFixedSize(550, 450);
+  right_controller->move(close_cont_right_x, close_cont_right_y);
+  right_controller->setStyleSheet("QWidget { background: url(../img/right_controller.png); }");
+  right_controller->stackUnder(turing_head);
+  right_controller->hide();
+
+  open_right_controller = new QPushButton(window);
+  open_right_controller->resize(50, 45);
+  open_right_controller->move(1020, 353);
+  open_right_controller->setStyleSheet("QPushButton { background: transparent;"
+                                      "color: #184C01;"
+                                      "font-size: 45px; } ");
+  open_right_controller->setText(">");
+  connect(open_right_controller, SIGNAL(released()), this, SLOT(edit_right_cont()));
 
   window->show();
 }
@@ -48,15 +72,31 @@ void TuringM::edit_left_cont() {
     left_controller->hide();
     left_controller->move(close_cont_left_x, close_cont_left_y);
     left_ear->move(close_ear_left_x, close_ear_left_y);
-    open_left_controller->move(415, 353);
+    open_left_controller->move(395, 353);
     open_left_controller->setText("<");
   } else {
     left_controller->show();
     left_controller->move(open_cont_left_x, open_cont_left_y);
     left_ear->move(open_ear_left_x, open_ear_left_y);
-    open_left_controller->move(37, 353);
+    open_left_controller->move(17, 353);
     open_left_controller->setText(">");
   }
   left_opened = !left_opened;
-  window->update();
+}
+
+void TuringM::edit_right_cont() {
+  if (right_opened) {
+    right_controller->hide();
+    right_controller->move(close_cont_right_x, close_cont_right_y);
+    right_ear->move(close_ear_right_x, close_ear_right_y);
+    open_right_controller->move(1020, 353);
+    open_right_controller->setText(">");
+  } else {
+    right_controller->show();
+    right_controller->move(open_cont_right_x, open_cont_right_y);
+    right_ear->move(open_ear_right_x, open_ear_right_y);
+    open_right_controller->move(1393, 353);
+    open_right_controller->setText("<");
+  }
+  right_opened = !right_opened;
 }
